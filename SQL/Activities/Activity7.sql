e237be1f74d23f94ef817c42bf89352614624fdc
Rugved Kubal <47049349+rugvedKubal@users.noreply.github.com>
2022-01-30 20:42:50 +0530

    Added SQL activities

---
+++ SQL/Activities/Activity7.sql
@@ -0,0 +1,9 @@
+SELECT SUM(purchase_amount) FROM orders;
+
+SELECT AVG(purchase_amount) FROM orders;
+
+SELECT MAX(purchase_amount) FROM orders;
+
+SELECT MIN(purchase_amount) FROM orders;
+
+SELECT COUNT(DISTINCT salesman_id) FROM orders;
